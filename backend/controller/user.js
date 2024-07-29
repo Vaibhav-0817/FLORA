253d54b934ee19e9fd17c0b033137ce79c799c67
vaibhav <vaibhavs00788@gmail.com>
2024-07-29 16:36:13 +0530

    cookie correction 2

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -138,6 +138,8 @@ router.get(
       res.cookie("token", null, {
         expires: new Date(Date.now()),
         httpOnly: true,
+        sameSite: "none",
+        secure: true,
       });
 
       res.status(201).json({
