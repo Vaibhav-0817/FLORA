d072ee7d116b14edb560c1417bd069c7ad3e825b
vaibhav <vaibhavs00788@gmail.com>
2024-07-29 14:58:52 +0530

    cors correction done

--- backend/App.js
+++ backend/App.js
@@ -5,7 +5,7 @@ const cookieParser = require('cookie-parser');
 const cors = require("cors");
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://flora-frontend-g2yg.onrender.com/',
   credentials: true,
 }));
 app.use(express.json({limit: '5mb'}));
